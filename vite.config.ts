b86fa841756b63a8c663a0540ec2d677a38e782f
zfitness <zfitness@163.com>
2024-01-17 12:41:02 +0800

    feat: init

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // base: '/webcontainer-font-carrier-demo/',
+  base: process.env.NODE_ENV === 'production' ? '/webcontainer-font-carrier-demo/' : '/',
   publicDir: '/webcontainer-font-carrier-demo/',
   plugins: [react()],
   server: {
