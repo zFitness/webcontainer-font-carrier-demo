5379e49ce7adeb7b02307bf3d9d6260207960c89
zfitness <zfitness@163.com>
2024-01-17 14:21:11 +0800

    feat: vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: '/webcontainer-font-carrier-demo/',
-  base: process.env.NODE_ENV === 'production' ? '/webcontainer-font-carrier-demo/' : '/',
+  base: './',
   publicDir: '/webcontainer-font-carrier-demo/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: './public/coi.min.js',
+          dest: './',
+        },
+      ],
+    }),
+  ],
+
   server: {
     headers: {
       'Cross-Origin-Embedder-Policy': 'require-corp',
